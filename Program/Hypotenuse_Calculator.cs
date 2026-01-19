2165649742edd55deddba9b770ec151e119752a9
EXO_Kaelin <134412829+Kaelinkes@users.noreply.github.com>
2026-01-19 14:16:16 +0200

    Add files via upload

--- Program/Hypotenuse_Calculator.cs
+++ Program/Hypotenuse_Calculator.cs
@@ -1,1 +1,23 @@
+﻿using System;
 
+namespace Hypotenuse_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a,b,c;
+
+            Console.WriteLine("Welcome to my Hypotenuse calculator");
+
+            Console.Write("Enter side A: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter side B: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            
+            c = Math.Sqrt(Math.Pow(a,2) + Math.Pow(b,2));
+            Console.WriteLine("The Hypotenuse is "+ c);
+        }
+    }
+}
